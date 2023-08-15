5bf6a9f22b5a0f58fcbff3e53fc7aab6558a5f68
MINTY <lmurte@hotmail.com>
2023-08-15 11:44:21 +0200

    Added Area class & AreaManager file system

--- src/xyz/mintydev/duels/core/Arena.java
+++ src/xyz/mintydev/duels/core/Arena.java
@@ -1,5 +1,50 @@
 package xyz.mintydev.duels.core;
 
+import org.bukkit.Location;
+import org.bukkit.World;
+
+import xyz.mintydev.duels.util.Cuboid;
+
 public class Arena {
 
+	private final String id, displayName;
+	private final World world;
+	private final Location spawn1, spawn2;
+	
+	private final Cuboid area;
+	
+	public Arena(String id, String displayName, World world, Location point1, Location point2, Location spawn1, Location spawn2) {
+		this.id = id;
+		this.displayName = displayName;
+		this.world = world;
+		this.spawn1 = spawn1;
+		this.spawn2 = spawn2;
+		
+		this.area = new Cuboid(point1, point2);
+	}
+
+	public String getId() {
+		return id;
+	}
+	
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public Cuboid getArea() {
+		return area;
+	}
+	
+	public Location getSpawn1() {
+		return spawn1;
+	}
+	
+	public Location getSpawn2() {
+		return spawn2;
+	}
+	
+	public World getWorld() {
+		return world;
+	}
+	
 }
--- src/xyz/mintydev/duels/managers/ArenaManager.java
+++ src/xyz/mintydev/duels/managers/ArenaManager.java
@@ -1,13 +1,55 @@
 package xyz.mintydev.duels.managers;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
 import xyz.mintydev.duels.MINTDuels;
 
 public class ArenaManager {
 
 	private final MINTDuels main;
 	
+	private static File customConfigFile;
+	private static FileConfiguration dataFile;
+	
+	private final String fileName = "arenas.yml";
+	
 	public ArenaManager(MINTDuels main) {
 		this.main = main;
+		createCustomConfig();
 	}
+	
+	private void loadArenas() {
+		
+	}
+	
+    private void createCustomConfig() {
+        customConfigFile = new File(main.getDataFolder(), fileName);
+        if (!customConfigFile.exists()) {
+            customConfigFile.getParentFile().mkdirs();
+            main.saveResource(fileName, false);
+         }
+
+        dataFile= new YamlConfiguration();
+        try {
+        	dataFile.load(customConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /* Getters & Setters */
+    
+    public static FileConfiguration getDataFile() {
+		return dataFile;
+	}
+    
+    public static File getCustomConfigFile() {
+		return customConfigFile;
+	}	
 	
 }
