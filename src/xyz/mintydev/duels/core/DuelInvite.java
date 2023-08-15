a8681e4422309690cbddd0e01a568b118f1329e5
MINTY <lmurte@hotmail.com>
2023-08-15 11:47:55 +0200

    Update DuelInvite.java

--- src/xyz/mintydev/duels/core/DuelInvite.java
+++ src/xyz/mintydev/duels/core/DuelInvite.java
@@ -1,5 +1,28 @@
 package xyz.mintydev.duels.core;
 
+import org.bukkit.entity.Player;
+
 public class DuelInvite {
 
+	private final Player sender, target;
+	private final Kit kit;
+	
+	public DuelInvite(Player sender, Player target, Kit kit) {
+		this.sender = sender;
+		this.target = target;
+		this.kit = kit;
+	}
+	
+	public Player getSender() {
+		return sender;
+	}
+	
+	public Player getTarget() {
+		return target;
+	}
+	
+	public Kit getKit() {
+		return kit;
+	}
+	
 }
